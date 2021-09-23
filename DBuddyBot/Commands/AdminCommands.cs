021f9dab371efac6467a7a44b50d7521fecd877f
pron1x <lukas98z@web.de>
2021-09-23 17:58:36 +0200

    Rewrite AddGame to add role to database if it already exists instead of exiting

--- DBuddyBot/Commands/AdminCommands.cs
+++ DBuddyBot/Commands/AdminCommands.cs
@@ -27,11 +27,16 @@ namespace DBuddyBot.Commands
         public async Task AddGame(CommandContext ctx, [RemainingText] string name)
         {
             name = name.ToTitleCase();
-            if (ctx.Guild.Roles.Any(r => r.Value.Name.ToLower() == name.ToLower()))
+            bool existsInDatabase = Database.TryGetGame(name, out Game game);
+            DiscordRole existsAsRole = ctx.Guild.Roles.FirstOrDefault(r => r.Value.Name.ToLower() == name.ToLower()).Value;
+            if (existsAsRole != null && !existsInDatabase)
             {
-                await ctx.Channel.SendMessageAsync($"A role named {name} already exists on the Server, will not create that again.");
+                game = new(existsAsRole.Name);
+                Database.AddGame(game);
+                await ctx.Channel.SendMessageAsync($"A role named {existsAsRole.Name} already exists on the Server, it has been added to the database.");
+                ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} added {game.Name} to database, role existed.");
             }
-            else if (!Database.TryGetGame(name, out Game game))
+            else if (!existsInDatabase)
             {
                 DiscordRole role = await ctx.Guild.CreateRoleAsync(name: name, color: DiscordColor.Purple, mentionable: true, reason: $"{ctx.Member.Nickname} added {name} to the game database.");
                 if (role != null)
@@ -39,7 +44,7 @@ namespace DBuddyBot.Commands
                     Game newGame = new(name);
                     Database.AddGame(newGame);
                     await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
-                    ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} added {newGame.Name} to database.");
+                    ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} added {newGame.Name} to database, new role has been created.");
                 }
             }
             else
@@ -48,6 +53,7 @@ namespace DBuddyBot.Commands
                 ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} tried to add {name} to database, but already exists.");
             }
         }
+
 
         /// <summary>
         /// Removes a game from the database and deletes the corresponding discord role
