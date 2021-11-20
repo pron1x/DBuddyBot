99d23de7508ad167927c1bc98d42b8a96be341ff
pron1x <lukas98z@web.de>
2021-11-20 13:38:16 +0100

    Remove unnecessary user commands

--- DBuddyBot/Commands/UserCommands.cs
+++ DBuddyBot/Commands/UserCommands.cs
@@ -19,60 +19,7 @@ namespace DBuddyBot.Commands
 
 
         #region commandmethods
-        /// <summary>
-        /// <c>AddGameToUser()</c> adds the game role to the user.
-        /// </summary>
-        /// <param name="ctx"></param>
-        /// <param name="name"></param>
-        /// <returns>Name of the game</returns>
-        [Command("get")]
-        public async Task AddGameToUser(CommandContext ctx, [RemainingText] string name)
-        {
-            name = name.ToTitleCase();
-            if (Database.TryGetRole(name, out Role game))
-            {
-                DiscordRole role = ctx.Guild.Roles.FirstOrDefault(x => x.Value.Name.ToLower() == game.Name.ToLower()).Value;
-                await ctx.Member.GrantRoleAsync(role, $"User added {game.Name} to their collection.");
-                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
-                ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} subscribed to {game.Name}.");
-            }
-            else
-            {
-                await ctx.Channel.SendMessageAsync($"The game {name} does not exist as a role yet. Ask an admin to add it to the collection!");
-                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":no_entry:"));
-                ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} tried to subscribe to {name}, but does not exist.");
-            }
-        }
-
-
-        /// <summary>
-        /// <c>RemoveGameFromUser</c> removes the game role from the user.
-        /// </summary>
-        /// <param name="ctx"></param>
-        /// <param name="name">Name of the game</param>
-        /// <returns></returns>
-        [Command("unsubscribe")]
-        public async Task RemoveGameFromUser(CommandContext ctx, [RemainingText] string name)
-        {
-            name = name.ToTitleCase();
-            if (Database.TryGetRole(name, out Role game))
-            {
-                DiscordRole role = ctx.Guild.Roles.FirstOrDefault(x => x.Value.Name.ToLower() == game.Name.ToLower()).Value;
-                if (ctx.Member.Roles.Contains(role))
-                {
-                    await ctx.Member.RevokeRoleAsync(role);
-                }
-                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
-                ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} unsubscribed from {game.Name}.");
-            }
-            else
-            {
-                await ctx.Channel.SendMessageAsync($"The game {name} does not exist as a role!");
-                ctx.Client.Logger.Log(LogLevel.Information, $"{ctx.Member.Username} tried to unsubscribe from {name}, but does not exist.");
-            }
-        }
-
-
+        
         /// <summary>
         /// <c>ShowGameInfo</c> shows info about the game.
         /// </summary>
