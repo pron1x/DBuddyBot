a01eb632f8fa2fb0c27aeaa9d7c39acd960bac31
pron1x <lukas98z@web.de>
2021-12-11 16:54:03 +0100

    Improve exception handling in ComponentInteractionHandler, interactions now timeout on errors.

--- DBuddyBot/EventHandlers/ComponentInteractionHandler.cs
+++ DBuddyBot/EventHandlers/ComponentInteractionHandler.cs
@@ -2,6 +2,8 @@
 using DBuddyBot.Models;
 using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
+using Serilog;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,30 +21,40 @@ namespace DBuddyBot.EventHandlers
 
         public static async Task HandleComponentInteraction(DiscordClient sender, DSharpPlus.EventArgs.ComponentInteractionCreateEventArgs e)
         {
-            await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
             Category category = _database.GetCategoryFromMessage(e.Message.Id);
             Role role = category?.GetRoleFromComponentId(e.Id);
             if (role != null)
             {
                 DiscordMember member = (DiscordMember)e.User;
                 DiscordRole discordRole = e.Guild.GetRole(role.DiscordId);
-                if (member.Roles.Contains(discordRole))
+                try
                 {
-                    try
+                    if (member.Roles.Contains(discordRole))
                     {
                         await member.RevokeRoleAsync(discordRole);
                     }
-                    catch { }
-                }
-                else
-                {
-                    try
+                    else
                     {
                         await member.GrantRoleAsync(discordRole);
                     }
-                    catch { }
+                }
+                catch (UnauthorizedException exception)
+                {
+                    Log.Logger.Error(exception, $"Not allowed to grant/revoke role {discordRole.Name} ({discordRole.Id}) on guild {e.Guild.Name} ({e.Guild.Id}).");
+                    return;
+                }
+                catch (NotFoundException exception)
+                {
+                    Log.Logger.Error(exception, $"Role {discordRole.Name} ({discordRole.Id}) was not found on guild {e.Guild.Name} ({e.Guild.Id}).");
+                    return;
+                }
+                catch (Exception exception)
+                {
+                    Log.Logger.Error(exception, "An error occured trying to grant/revoke a role.");
+                    return;
                 }
             }
+            await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
         }
     }
 }
