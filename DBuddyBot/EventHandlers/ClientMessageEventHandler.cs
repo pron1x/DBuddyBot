4dd5c656e7a66fe5f1d940bc35a8244d1e8a5c9d
pron1x <lukas98z@web.de>
2021-12-14 19:34:47 +0100

    Add handling of role message removal

---
+++ DBuddyBot/EventHandlers/ClientMessageEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using DBuddyBot.Data;
+using DBuddyBot.Models;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System.Threading.Tasks;
+
+namespace DBuddyBot.EventHandlers
+{
+    public class ClientMessageEventHandler
+    {
+        private static readonly IDatabaseService _database;
+
+        static ClientMessageEventHandler()
+        {
+            _database = Bootstrapper.Database;
+        }
+
+        internal static Task RemoveRoleMessage(DiscordClient sender, MessageDeleteEventArgs e)
+        {
+            return Task.Run(() =>
+            {
+                Category category;
+                if ((category = _database.GetCategoryFromMessage(e.Message.Id)) != null)
+                {
+                    _database.UpdateMessage(category.Id, 0);
+                }
+            });
+        }
+    }
+}
