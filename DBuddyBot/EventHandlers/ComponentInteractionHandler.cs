a808cd23636d4bcfb3bcac47104849235a7806aa
pron1x <lukas98z@web.de>
2021-12-15 18:12:03 +0100

    Add ephemeral response to component interaction when removing/adding roles

--- DBuddyBot/EventHandlers/ComponentInteractionHandler.cs
+++ DBuddyBot/EventHandlers/ComponentInteractionHandler.cs
@@ -23,6 +23,9 @@ namespace DBuddyBot.EventHandlers
         {
             return Task.Run(async () =>
             {
+                await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder() { IsEphemeral = true }); ;
+                string description = string.Empty;
+                DiscordColor color = DiscordColor.DarkGreen;
                 Category category = _database.GetCategoryFromMessage(e.Message.Id);
                 Role role = category?.GetRoleFromComponentId(e.Id);
                 if (role != null)
@@ -34,29 +37,40 @@ namespace DBuddyBot.EventHandlers
                         if (member.Roles.Contains(discordRole))
                         {
                             await member.RevokeRoleAsync(discordRole);
+                            description = $"Removed {discordRole.Mention}";
                         }
                         else
                         {
                             await member.GrantRoleAsync(discordRole);
+                            description = $"Added {discordRole.Mention}";
                         }
                     }
                     catch (UnauthorizedException exception)
                     {
                         Log.Logger.Error(exception, $"Not allowed to grant/revoke role {discordRole.Name} ({discordRole.Id}) on guild {e.Guild.Name} ({e.Guild.Id}).");
-                        return;
+                        color = DiscordColor.DarkRed;
+                        description = $"Can't manage the role {discordRole.Mention}. Please let the admins know!";
                     }
                     catch (NotFoundException exception)
                     {
                         Log.Logger.Error(exception, $"Role {discordRole.Name} ({discordRole.Id}) was not found on guild {e.Guild.Name} ({e.Guild.Id}).");
-                        return;
+                        color = DiscordColor.DarkRed;
+                        description = $"Role {discordRole.Name} was not found. Please let the admins know!";
                     }
                     catch (Exception exception)
                     {
                         Log.Logger.Error(exception, "An error occured trying to grant/revoke a role.");
-                        return;
+                        color = DiscordColor.DarkRed;
+                        description = $"Something went wrong. Please let the admins know!";
+                    }
+                    finally
+                    {
+                        DiscordEmbedBuilder embed = new();
+                        embed.Color = color;
+                        embed.Description = description;
+                        await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder() { Content = string.Empty }.AddEmbed(embed.Build()));
                     }
                 }
-                await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
             });
         }
     }
